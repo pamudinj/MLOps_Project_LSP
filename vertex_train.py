1906cb55c5b27679213b83af93f8b09cef914c9d
pamudinj <pamudinj@gmail.com>
2026-07-15 17:14:17 +0200

    modified vertex training

--- vertex_train.py
+++ vertex_train.py
@@ -74,7 +74,7 @@ def main() -> None:
                 "WANDB_ENTITY": os.getenv("WANDB_ENTITY", ""),
                 "WANDB_PROJECT": os.getenv("WANDB_PROJECT", ""),
             },
-            sync=True,
+            sync=False,
         )
 
 
